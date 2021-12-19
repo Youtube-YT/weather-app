cb280fe79638da0d7be3068df4320f21e834c23b
Youtube-YT <mingweiop@gmail.com>
2021-12-19 03:01:55 +0000

    commit 5

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,6 +20,5 @@ async function getWeather() {
     <input id="location" bind:value={location} type="text" />
     <button type="submit">Check</button>
 </form>
-<p>
-Temparature: </p> temparature
-
+<p>Temperature: {temperature}</p>
+<p>Description: {description}</p>
