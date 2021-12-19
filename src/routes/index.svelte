b874fc6dc6b2f83a45c44b632cbd3d622a4c2e13
Youtube-YT <mingweiop@gmail.com>
2021-12-19 02:43:16 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<script>
+let location = "";
+let temparature = "";
+let description = ;
+    </script>
+
+<h1>Weather App</h1>
+
+<form on:submit|preventDefault={getWeather}>
+    <label for="location">Location:</label>
+    <input id="location" bind:value={location} type="text" />
+    <button type="submit">Check</button>
+</form>
+
