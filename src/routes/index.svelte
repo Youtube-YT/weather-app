12b8ae965d5f66c54aedbea664348425cbc8566d
Youtube-YT <mingweiop@gmail.com>
2021-12-19 02:58:45 +0000

    commit 2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,20 @@
 let location = "";
 let temparature = "";
 let description = ;
+async function getWeather() {
+  fetch('https://goweather.herokuapp.com/weather/' + location)
+	.then((response) => response.json())
+	.then((data) => {
+  	//Able to change whatever name you like Example: weatherTemparature = data.temperature the name of data should not be changed.
+       temperature = data.temperature; // Path for temperature
+  	description = data.description; // Path for description
+	});
+}
     </script>
 
 <h1>Weather App</h1>
 
-<form on:submit|preventDefault={getWeather}>
+<form on:submit|preventDefault={getWeather}>    
     <label for="location">Location:</label>
     <input id="location" bind:value={location} type="text" />
     <button type="submit">Check</button>
